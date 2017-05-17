8f3432d14fc09bab5f40b7dc142b087b10f3723a
Andreas Beyl <ab.gitlab@manywaysout.de>
2017-05-17 17:18:06 +0200

    [BUGIFX] change tca-settings to enable v8 compatibility

--- ext_tables.php
+++ ext_tables.php
@@ -11,31 +11,4 @@ if (!defined('TYPO3_MODE')) {
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_a21glossary_main');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToInsertRecords('tx_a21glossary_main');
 
-$TCA['tx_a21glossary_main'] = array(
-	'ctrl' => array(
-		'title' => 'LLL:EXT:a21glossary/locallang_db.xml:tx_a21glossary_main',
-		'label' => 'short',
-		'tstamp' => 'tstamp',
-		'crdate' => 'crdate',
-		'cruser_id' => 'cruser_id',
-		'versioning' => '1',
-		'languageField' => 'sys_language_uid',
-		'transOrigPointerField' => 'l18n_parent',
-		'transOrigDiffSourceField' => 'l18n_diffsource',
-		'default_sortby' => 'ORDER BY short,uid',
-		'delete' => 'deleted',
-		'enablecolumns' => array(
-			'disabled' => 'hidden',
-			'starttime' => 'starttime',
-			'endtime' => 'endtime',
-			'fe_group' => 'fe_group'
-		),
-		'dynamicConfigFile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/Tca.php',
-		'iconfile' => 'EXT:a21glossary/icon_tx_a21glossary_main.gif',
-	),
-	'feInterface' => array(
-		'fe_admin_fieldList' => 'sys_language_uid, l18n_parent, l18n_diffsource, hidden, starttime, endtime, fe_group, short, shortcut, longversion, shorttype, language, description, link, exclude, force_linking, force_case, force_preservecase, force_regexp, force_global',
-	)
-);
-
 ?>
